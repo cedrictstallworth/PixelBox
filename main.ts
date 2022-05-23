843b5315197b073ab13e17cdd5bd4d35db1c258c
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-23 01:59:03 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace pixelboxExtension{
+    /**
+     * * Address LEDs with linear index
+     */
+    //% blockId=pixelboxExtensionplotAt
+    //% block='plot at index"
+    //% index.min=0 index.max=25
     export function plotAt(index: number): void {
         const y = Math.floor(index / 5);
         const x = index % 5;
