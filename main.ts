8a2aaa4d2e4631ed366b5aaaa49831f743c8b209
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-23 05:27:16 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-enum pixels{
-    //% block ="$c00"
-    //% c00.shadow="colorNumberPicker",
-
-    //% block= "$c01" 
-    //% c01.shadow="colorNumberPicker"
-
-
-}
-
 //%color="#4B0082" icon="\u2592"
 namespace PixelBox{
     /**
@@ -34,15 +24,33 @@ namespace PixelBox{
      */
     
     //% blockId=PixelBox_setPixel
-    
 
-    
-    //% block
-    //% pixels.fieldEditor="gridpicker"
-    //% pixels.fieldOptions.width=220
-    //% pixels.fieldOptions.columns=8
-    
+    //% block ="$c00"
+    //% block= "$c01" 
+    //% block= "$c02" 
+    //% block= "$c03" 
+    //% block= "$c04" 
+    //% block= "$c05" 
+    //% block= "$c06" 
+    //% block= "$c07"
+    //% block ="$c08" 
+    //% block= "$c09" 
+    //% block= "$c10" 
+    //% block= "$c11" 
+    //% block= "$c12" 
+    //% block= "$c13" 
+    //% block= "$c14" 
+    //% block= "$c15"
 
+    //% c00.shadow="colorNumberPicker"
+    //% c01.shadow="colorNumberPicker"
+    //% c02.shadow="colorNumberPicker"
+    //% c03.shadow="colorNumberPicker"
+    //% c04.shadow="colorNumberPicker"
+    //% c05.shadow="colorNumberPicker"
+    //% c06.shadow="colorNumberPicker"
+    //% c07.shadow="colorNumberPicker"
+    //% c08.shadow="colorNumberPicker"
 
      export function setPixel(
          c00: number, c01: number, c02: number, c03: number, c04: number, c05: number, c06: number, c07: number,
