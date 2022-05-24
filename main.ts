bd4ae0b2eadeaa5c688c8182f2b4d52d82a57adf
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-24 07:35:29 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -31,7 +31,8 @@ namespace PixelBox{
          * @param leds the pattern of LED to turn on/off
          * @param interval time in milliseconds to pause after drawing
          */
-    /*
+    
+    /** 
     //% help=basic/show-leds
     //% weight=95 blockGap=8
     //% imageLiteral=1 async
