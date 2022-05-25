3a0eef2cd511da34b41ea03f012b7904ac6e1a23
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-25 19:16:02 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,22 @@ namespace PixelBox{
     pixelbox.setMatrixWidth(8);
     pixelbox.clear();
 
+    /**
+         * *Set pixel image ROWXX by clicking on pixels and selecting colors
+         */
+    //% blockId=Pixelbox_setPixelboxColorsForRow
+    //% block ="set pixelbox colors for row $row, $c00 $c01 $c02 $c03 $c04 $c05 $c06 $c07"
+    //% inlineInputMode=inline
+    //% row.min=0 row.max=7
 
+    //% c00.shadow="colorNumberPicker"
+    //% c01.shadow="colorNumberPicker"
+    //% c02.shadow="colorNumberPicker"
+    //% c03.shadow="colorNumberPicker"
+    //% c04.shadow="colorNumberPicker"
+    //% c05.shadow="colorNumberPicker"
+    //% c06.shadow="colorNumberPicker"
+    //% c07.shadow="colorNumberPicker"
 
 
 }
