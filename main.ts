e7fcad8d5534609b9381ad6a1c2ca7ef7b8cbe15
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-25 19:01:11 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,32 +8,7 @@ namespace Pixelbox{
     pixelbox.setMatrixWidth(8);
     pixelbox.clear();
  
-    /**TEST FOR A BETTER WAY *************************************************************************
-         * *Set pixel image ROWXX by clicking on pixels and selecting colors
-         */
-    //% blockId=Pixelbox_setPixelboxColorsForRow
-    //% block ="set pixel colors for row $row, $c00 $c01 $c02 $c03 $c04 $c05 $c06 $c07"
-    //% inlineInputMode=inline
-    //% row.min=0 row.max=7
-    //% weight=50
 
-    //% c00.shadow="colorNumberPicker"
-    //% c01.shadow="colorNumberPicker"
-    //% c02.shadow="colorNumberPicker"
-    //% c03.shadow="colorNumberPicker"
-    //% c04.shadow="colorNumberPicker"
-    //% c05.shadow="colorNumberPicker"
-    //% c06.shadow="colorNumberPicker"
-    //% c07.shadow="colorNumberPicker"
-
-    export function setPixelboxColorsForRow(row: number,
-        c00: number, c01: number, c02: number, c03: number, c04: number, c05: number, c06: number, c07: number)
-        : void {
-        let colors = [c00, c01, ,c02, c03, c04, c05, c06, c07];   
-        for (let column = 0; column<=7;  column++){
-            pixelbox.setMatrixColor(column, row, neopixel.colors(colors[column]));
-        }
-    }
 
     /***************************************************************************
          * *Show Pixelbox Image
