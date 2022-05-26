b174e4cb00b35165af086b6e8b10ab40041ba10b
Cedric T. Stallworth <cedrictstallworth@gmail.com>
2022-05-25 21:16:39 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ namespace PixelBox{
          * *Set pixel image ROWXX by clicking on pixels and selecting colors
          */
     //% blockId=Pixelbox_setPixelboxColorsForRow
-    //% block ="set pixelbox colors for row $row, $c00 $c01 $c02 $c03 $c04 $c05 $c06 $c07"
+    //% block ="set image $image colors for row $row, $c00 $c01 $c02 $c03 $c04 $c05 $c06 $c07"
     //% inlineInputMode=inline
     //% row.min=0 row.max=7
 
@@ -40,12 +40,12 @@ namespace PixelBox{
     //% c06.shadow="colorNumberPicker"
     //% c07.shadow="colorNumberPicker"
 
-    export function setPixelboxColorsForRow(row: number,
+    export function setPixelboxColorsForRow(image: PixelBoxImage, row: number,
         c00: number, c01: number, c02: number, c03: number, c04: number, c05: number, c06: number, c07: number)
         : void {
         let colors = [c00, c01, c02, c03, c04, c05, c06, c07];
         for (let column = 0; column <= 7; column++) {
-            pixelbox.setMatrixColor(column, row, neopixel.colors(colors[column]));
+            image.pixel.setMatrixColor(column, row, neopixel.colors(colors[column]));
         }
     }
 
@@ -53,10 +53,10 @@ namespace PixelBox{
          * *Set pixel image ROWXX by clicking on pixels and selecting colors
          */
     //% blockId=Pixelbox_showPixelBoxImage
-    //% block ="show pixelbox image"
+    //% block ="show pixelbox image $image"
 
-    export function showPixelBoxImage(): void {
-        pixelbox.show();
+    export function showPixelBoxImage(image: PixelBoxImage): void {
+        image.pixel.show();
     }
 
 
